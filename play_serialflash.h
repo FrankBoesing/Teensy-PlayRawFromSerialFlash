0f6bbe8f528f5ab4903e38e38b6cb33bdca686b0
Frank Bösing <f.boesing@gmx.de>
2014-12-30 22:02:05 +0100

    Initial Commit

---
+++ play_serialflash.h
@@ -0,0 +1,57 @@
+/* Audio Library for Teensy 3.X
+ * Copyright (c) 2014, Paul Stoffregen, paul@pjrc.com
+ *
+ * Development of this audio library was funded by PJRC.COM, LLC by sales of
+ * Teensy and Audio Adaptor boards.  Please support PJRC's efforts to develop
+ * open source software by purchasing Teensy or other PJRC products.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice, development funding notice, and this permission
+ * notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+// Modified to play from Serial Flash (c) Frank Bösing, 2014/12
+ 
+#ifndef play_serialflash_h_
+#define play_serialflash_h_
+
+#include "AudioStream.h"
+#include "flash_spi.h"
+
+#define SERFLASH_CS 				6	//Chip Select W25Q128FV SPI Flash
+
+class AudioPlaySerialFlash : public AudioStream
+{
+public:
+	AudioPlaySerialFlash(void) : AudioStream(0, NULL), playing(0) { flash_init(); }
+	void play(const unsigned int data);
+	void stop(void);
+	bool isPlaying(void) { return playing; }
+	uint32_t positionMillis(void);
+	uint32_t lengthMillis(void);
+	virtual void update(void);
+private:
+	unsigned int next;
+	unsigned int beginning;
+	unsigned int blocknr;
+	uint32_t length;
+	int16_t prior;
+	volatile uint8_t playing;
+	unsigned int buffer[128];
+};
+
+#endif
